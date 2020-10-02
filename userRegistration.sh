8b2ebca4163215897c5850ff19056cc4cc08a584
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-02 22:41:06 +0530

    Added : FirstName Validation functionality

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to User Registration Form"
+
+read -p "Enter First Name:" firstName;
+
+pattern="^[A-Z]{1}[a-zA-Z]{2,}$";
+
+if [[ $firstName =~ $pattern ]];
+then
+	echo "valid first name";
+else
+	echo "invalid first name";
+fi
