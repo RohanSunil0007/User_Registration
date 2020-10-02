296c1f5b84ea1680c10c44e678a4b1654de4e0f4
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-02 23:33:57 +0530

    Added : Password rule 1 functionality

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,11 +5,13 @@ read -p "Enter First Name:" firstName
 read -p "Enter Last Name:" lastName
 read -p "Enter E-mail:" email
 read -p "Enter Mobile no:" mobileNumber
+read -p "Enter Password:" password
 
 pattern="^[A-Z]{1}[a-zA-Z]{2,}$"
 pattern1="^[A-Z]{1}[a-z A-Z]{2,}$"
 pattern2="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-z]{2,3})*$"
 pattern3="^([9]{1}[1]{1}) [0-9]{10}$"
+pattern4="^[0-9a-zA-z]{8,}$"
 
 #FirstName
 if [[ $firstName =~ $pattern ]]
@@ -41,4 +43,12 @@ then
 	echo "valid mobile number";
 else
 	echo "Invalid mobile number!!!";
+fi
+
+#Password
+if [[ $password =~ $pattern4 ]];
+then
+	echo "valid password";
+else
+	echo "Invalid password!!!";
 fi
